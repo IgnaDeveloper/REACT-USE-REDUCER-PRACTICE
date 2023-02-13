bc2d41ee01ab34e384255e7ce79aa4c302362029
IgnaDeveloper <ignadeveloper@gmail.com>
2023-02-12 22:40:47 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
 
 function App() {
+  const meses = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
+
+  const fechaActual = new Date();
+  const yearActual = fechaActual.getFullYear();
+  const monthActual = fechaActual.getMonth();
+
+  const initialState = {
+    year: yearActual,
+    month: monthActual,
+  };
+
+  const reducer = (state, action) => {
+    let newYear = state.year;
+    let newMonth = state.month;
+
+    switch (action.type) {
+      case "masM":
+        newMonth = newMonth == 11 ? 0 : newMonth + 1;
+        break;
+      case "masY":
+        newYear = newYear + 1;
+        break;
+      case "menosM":
+        newMonth = newMonth == 0 ? 11 : newMonth - 1;
+        break;
+      case "menosY":
+        newYear = newYear - 1;
+        break;
+    }
+    return { year: newYear, month: newMonth };
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <h1>Contador Reducer</h1>
+        <h3
+          style={
+            (state.year < yearActual && state.month < monthActual) ||
+            state.year < yearActual
+              ? { color: "red" }
+              : { color: "green" }
+          }
         >
-          Learn React
-        </a>
-      </header>
+          {meses[state.month]} ({state.year})
+        </h3>
+        <div className="boxed">
+          <h4>Mes</h4>
+          <button onClick={() => dispatch({ type: "masM" })}>+</button>
+          <button onClick={() => dispatch({ type: "menosM" })}>-</button>
+        </div>
+        <div className="boxed">
+          <h4>Año</h4>
+          <button onClick={() => dispatch({ type: "masY" })}>+</button>
+          <button onClick={() => dispatch({ type: "menosY" })}>-</button>
+        </div>
+      </div>
     </div>
   );
 }
